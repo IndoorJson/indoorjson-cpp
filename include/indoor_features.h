cbdd8c6e5f5c750b5806ed0ede5ada348a2ba086
Kunlin Yu <yukunlin@syriusrobotics.com>
2023-12-20 18:51:56 +0800

    Add CMakeLists.txt and first class
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

---
+++ include/indoor_features.h
@@ -0,0 +1,16 @@
+/*
+ * indoor_features.h
+ *
+ * Copyright (c) 2023 IndoorJson
+ *
+ * Author: Kunlin Yu <yukunlin@syriusrobotics.com>
+ * Create Date: 2023/12/20
+ *
+ */
+
+namespace IndoorJson {
+
+class IndoorFeatures {
+};
+
+}  // namespace IndoorJson
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ * Copyright (c) 2023 IndoorJson
+ *
+ * Author: Kunlin Yu <yukunlin@syriusrobotics.com>
+ * Create Date: 2023/12/20
+ *
+ */
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, IndoorJson!" << std::endl;
+  return 0;
+}
