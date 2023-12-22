b71dca97a5105792c86704d02537677ed6ac4a21
Kunlin Yu <yukunlin@syriusrobotics.com>
2023-12-22 10:54:55 +0800

    Integrate libgeos
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
  *
  */
 
+#include <geos/geom/Geometry.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/io/WKTWriter.h>
+
 #include <iostream>
 #include <nlohmann/json-schema.hpp>
 
@@ -16,35 +20,46 @@ using nlohmann::json_schema::json_validator;
 
 static const json rectangle_schema = R"(
 {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "title": "A rectangle",
-    "properties": {
-        "width": {
-            "$ref": "#/definitions/length",
-            "default": 20
-        },
-        "height": {
-            "$ref": "#/definitions/length"
-        }
-    },
-    "definitions": {
-        "length": {
-            "type": "integer",
-            "minimum": 1,
-            "default": 10
-        }
-    }
-})"_json;
+     "$schema": "http://json-schema.org/draft-07/schema#",
+     "title": "A rectangle",
+     "properties": {
+         "width": {
+             "$ref": "#/definitions/length",
+             "default": 20
+         },
+         "height": {
+             "$ref": "#/definitions/length"
+         }
+     },
+     "definitions": {
+         "length": {
+             "type": "integer",
+             "minimum": 1,
+             "default": 10
+         }
+     }
+ })"_json;
 
-int main()
-{
+int main() {
+  auto factory = geos::geom::GeometryFactory::create();
+
+  std::initializer_list<geos::geom::Coordinate> coordList = {
+      {1.0, 2.0}, {4.0, 5.0}, {7.0, 8.0}};
+  geos::geom::CoordinateSequence cs(coordList);
+
+  auto ls = factory->createLineString(cs);
+
+  geos::io::WKTWriter writer;
+  std::cout << writer.write(ls.get()) << std::endl;
+
   try {
     json_validator validator{rectangle_schema};
-    /* validate empty json -> will be expanded by the default values defined in the schema */
+    /* validate empty json -> will be expanded by the default values defined
+    in the schema */
     json rectangle = "{}"_json;
     const auto default_patch = validator.validate(rectangle);
     rectangle = rectangle.patch(default_patch);
-    std::cout << rectangle.dump() << std::endl; // {"height":10,"width":20}
+    std::cout << rectangle.dump() << std::endl;  // {"height":10,"width":20}
   } catch (const std::exception &e) {
     std::cerr << "Validation of schema failed: " << e.what() << "\n";
     return EXIT_FAILURE;
